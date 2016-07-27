eb71d0fb9e3e0b00e8768c12e585d28de7295089
Jake Petroules <jake.petroules@petroules.com>
2016-07-26 20:53:50 -0700

    Initial Commit

---
+++ HTTPServer/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HTTPServer
+//
+//  Created by Jake Petroules on 7/26/16.
+//  Copyright © 2016 Jake Petroules. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
